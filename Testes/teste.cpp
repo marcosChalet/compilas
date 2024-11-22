a0999afd150b9192797d7bcfc87f4b030e3cf982
marcosChalet <chaletmarcos@gmail.com>
2024-11-22 11:47:44 -0300

    teste da matriz com "for loop"

--- Testes/teste.cpp
+++ Testes/teste.cpp
@@ -6,26 +6,16 @@ int matA[3:3]
 int matB[3:3]
 int matR[3:3]
 
-i = 0
-j = 0
-while (i < 3) {
-    while (j < 3) {
+for ( i = 0 ; i < 3; i = i + 1) {
+    for ( j = 0 ; j < 3; j = j + 1) {
         matR[i:j] = 0
-        j = j + 1
     }
-    i = i + 1
 }
 
-i = 0
-while (i < 3) {
-    j = 0
-    while (j < 3) {
-        k = 0
-        while (k < 3) {
+for ( i = 0 ; i < 3; i = i + 1) {
+    for ( j = 0 ; j < 3; j = j + 1) {
+        for ( k = 0 ; k < 3; k = k + 1) {
             matR[i:j] = matR[i:j] + matA[i:k] * matB[k:j]
-            k = k + 1
         }
-        j = j + 1
     }
-    i = i + 1
 }
