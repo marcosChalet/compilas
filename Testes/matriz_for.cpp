e2e739cfc0ae8fbb46a96cd791442521e0f8f306
marcosChalet <chaletmarcos@gmail.com>
2024-11-23 15:33:21 -0300

    correção exemplo

--- Testes/matriz_for.cpp
+++ Testes/matriz_for.cpp
@@ -8,12 +8,6 @@ int matR[3:3]
 
 for ( i = 0 ; i < 3; i = i + 1) {
     for ( j = 0 ; j < 3; j = j + 1) {
-        matR[i:j] = 0
-    }
-}
-
-for ( i = 0 ; i < 3; i = i + 1) {
-    for ( j = 0 ; j < 3; j = j + 1) {
         for ( k = 0 ; k < 3; k = k + 1) {
             matR[i:j] = matR[i:j] + matA[i:k] * matB[k:j]
         }
