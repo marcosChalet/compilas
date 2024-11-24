ee0f54d815157d8bce4fd2672d78756c8289b52f
Thiago Carlos <thiagocarlossilvapereira@gmail.com>
2024-11-24 20:20:16 -0300

    Declaração de função

---
+++ Debug/CMakeFiles/tradutor.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/thcls/Desktop/compilas/ast.cpp" "CMakeFiles/tradutor.dir/ast.cpp.o" "gcc" "CMakeFiles/tradutor.dir/ast.cpp.o.d"
+  "/home/thcls/Desktop/compilas/checker.cpp" "CMakeFiles/tradutor.dir/checker.cpp.o" "gcc" "CMakeFiles/tradutor.dir/checker.cpp.o.d"
+  "/home/thcls/Desktop/compilas/error.cpp" "CMakeFiles/tradutor.dir/error.cpp.o" "gcc" "CMakeFiles/tradutor.dir/error.cpp.o.d"
+  "/home/thcls/Desktop/compilas/gen.cpp" "CMakeFiles/tradutor.dir/gen.cpp.o" "gcc" "CMakeFiles/tradutor.dir/gen.cpp.o.d"
+  "/home/thcls/Desktop/compilas/lexer.cpp" "CMakeFiles/tradutor.dir/lexer.cpp.o" "gcc" "CMakeFiles/tradutor.dir/lexer.cpp.o.d"
+  "/home/thcls/Desktop/compilas/parser.cpp" "CMakeFiles/tradutor.dir/parser.cpp.o" "gcc" "CMakeFiles/tradutor.dir/parser.cpp.o.d"
+  "/home/thcls/Desktop/compilas/symtable.cpp" "CMakeFiles/tradutor.dir/symtable.cpp.o" "gcc" "CMakeFiles/tradutor.dir/symtable.cpp.o.d"
+  "/home/thcls/Desktop/compilas/tradutor.cpp" "CMakeFiles/tradutor.dir/tradutor.cpp.o" "gcc" "CMakeFiles/tradutor.dir/tradutor.cpp.o.d"
+  "" "tradutor" "gcc" "CMakeFiles/tradutor.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Debug/CMakeFiles/tradutor.dir/link.d
@@ -0,0 +1,28 @@
+tradutor: \
+  /usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libgcc_s.so \
+  /usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libgcc_s.so \
+  /usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libgcc_s.so \
+  /usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libgcc_s.so \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-unknown-linux-gnu/14.2.0/../../../x86_64-linux-gnu/libgcc_s.so:
+
+/lib64/ld-linux-x86-64.so.2:
